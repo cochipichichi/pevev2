88d6def0aedbfc19cd7fa1b644efc1560707348e
Ragnar <cochipichichi@gmail.com>
2025-12-15 20:17:25 -0300

    Add files via upload

---
+++ content/7basico/cienciasnaturales/ciencias-naturales-fisica/js/config.js
@@ -0,0 +1,2 @@
+export const SHEETS_WRITE_ENDPOINT = "https://script.google.com/macros/s/TU_ID/exec";
+export const SHEETS_READ_ENDPOINT  = "https://script.google.com/macros/s/TU_ID/exec?mode=read";
